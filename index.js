24634decdcaa367666dfd7b0f577bdc8d46e97ee
wolfiee42 <saifalislam2022@gmail.com>
2023-10-19 13:08:30 +0600

    initia server created

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require('express');
-const app = express()
+const app = express();
+const cors = require('cors');
+const port = process.nextTick.PORT || 5000;
+
+// middlewire
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send("sup niggas")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+})

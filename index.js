2779f2d12d8abc54950154c527c1119cf0c87bbd
wolfiee42 <saifalislam2022@gmail.com>
2023-10-19 19:29:57 +0600

    took the info and stored in cardetails

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const port = process.nextTick.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+
+// saifalislam2022
+// Eud9TrV7fixffY1b
+
+
+app.get('/', (req, res) => {
     res.send("sup niggas")
+})
+
+app.post('/cardetails', (req, res) => {
+    const carinfo = req.body;
+    console.log(carinfo);
 })
 
 

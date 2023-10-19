697000b9ea6a4ba916a3925e1230b82c71deb6ad
wolfiee42 <saifalislam2022@gmail.com>
2023-10-19 22:42:20 +0600

    adding car details

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
+
 const port = process.nextTick.PORT || 5000;
 
 // middlewire
@@ -8,18 +11,53 @@ app.use(cors());
 app.use(express.json());
 
 
-// saifalislam2022
-// Eud9TrV7fixffY1b
 
 
-app.get('/', (req, res) => {
-    res.send("sup niggas")
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@wolfiecar.hpdcarw.mongodb.net/?retryWrites=true&w=majority`;
 
-app.post('/cardetails', (req, res) => {
-    const carinfo = req.body;
-    console.log(carinfo);
-})
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    const carCollection = client.db("carDB").collection("cars");
+    try {
+        app.get('/', (req, res) => {
+            res.send("sup niggas")
+        })
+
+        app.post('/cardetails', async (req, res) => {
+            const carinfo = req.body;
+            const cursor = await carCollection.insertOne(carinfo);
+            res.send(cursor);
+            console.log(cursor);
+        })
+        app.get('/cardetails', async (req, res) => {
+            const result = await carCollection.find().toArray();
+            res.send(result);
+            console.log(result);
+        })
+
+
+
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 
 

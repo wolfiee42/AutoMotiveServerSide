c96d67a61376ed1236ed3a71904b8d3b693b645f
wolfiee42 <saifalislam2022@gmail.com>
2023-10-20 15:27:38 +0600

    delete and update created

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
     const carCollection = client.db("carDB").collection("cars");
     try {
-        
+
         app.get('/', (req, res) => {
             res.send("sup niggas")
         })
@@ -61,8 +61,10 @@ async function run() {
             console.log(result);
         })
 
+
+
         app.get('/cardetails/:brand/:id', async (req, res) => {
-            const brandname = req.params.brand ;
+            const brandname = req.params.brand;
             const queryb = { brand: brandname };
             const id = req.params.id
             const queryi = { _id: new ObjectId(id) };
@@ -71,6 +73,37 @@ async function run() {
             console.log(result);
         })
 
+
+        app.delete('/cardetails/:brand/:id', async (req, res) => {
+            const brandname = req.params.brand;
+            const queryb = { brand: brandname };
+            const id = req.params.id
+            const queryi = { _id: new ObjectId(id) };
+            const result = await carCollection.deleteOne(queryi, queryb);
+            res.send(result);
+            console.log(result);
+        })
+
+        // app.put('/cardetails/:brand/:id', async (req, res) => {
+        //     const brandname = req.params.brand;
+        //     const car = req.body
+        //     const id = req.params.id;
+        //     const queryi = { _id: new ObjectId(id) };
+        //     const queryb = { brand: brandname };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             name: car.name,
+        //             imageurl: car.imageurl,
+        //             price: car.price,
+        //             description: car.description,
+        //             brand: car.brand
+        //         },
+        //       };
+        //       const result = await carCollection.updateOne(queryi, queryb, updateDoc, options);
+        //       res.send(result);
+        //       console.log(result);
+        // })
 
 
         await client.connect();
